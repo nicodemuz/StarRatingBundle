326fbfcd3cb7cff931026109e9876b516f6f4d96
Pandy Legend <pandylegend@gmail.com>
2021-02-22 15:10:08 +0000

    Update StarRatingExtension.php to work with Twig 3

--- Extensions/StarRatingExtension.php
+++ Extensions/StarRatingExtension.php
@@ -3,8 +3,10 @@
 namespace Brokoskokoli\StarRatingBundle\Extensions;
 
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
 
-class StarRatingExtension extends \Twig_Extension
+class StarRatingExtension extends AbstractExtension
 {
 
     protected $container;
@@ -17,7 +19,7 @@ class StarRatingExtension extends \Twig_Extension
     public function getFilters()
     {
         return array(
-            new \Twig_SimpleFilter('rating', array($this, 'rating'), array('is_safe' => array('all'))),
+            new TwigFilter('rating', array($this, 'rating'), array('is_safe' => array('all'))),
         );
     }
 
